0d052633e76c53640c1bbf1866cf4f2cd93b6c5a
Brayden Vernon <4933@holbertonstudents.com>
2023-01-13 17:12:55 +0000

    fixed linter error in 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(studentList, city){
+export default function getStudentsByLocation(studentList, city) {
   return studentList.filter((obj) => obj.location === city);
 }
