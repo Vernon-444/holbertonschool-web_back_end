bfca77476fbdbc724b1453f50960e8ed6d4da0f5
Brayden Vernon <mt.vernon44@gmail.com>
2023-05-08 17:55:27 +0000

    fixed linter violation

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -8,7 +8,8 @@ import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """takes an int and waits for a random delay between 0 and max_delay seconds and returns"""
+    """takes an int and waits for a random delay between
+    0 and max_delay seconds and returns"""
     randFloat = random.uniform(0, max_delay)
     await asyncio.sleep(randFloat)
     return randFloat
