f80b73a181146df923b226ad74f8fde632f0788e
Brayden Vernon <mt.vernon44@gmail.com>
2023-05-08 13:38:13 +0000

    import tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """takes string k and an int/float v as arg and returns tuple"""
+from typing import Tuple
 
-def to_kv(k: str, v: float) -> tuple:
+
+def to_kv(k: str, v: float) -> Tuple[str, float]:
     """return tuple (k, v)"""
-    return (k, v)
+    return (k, v * v)
