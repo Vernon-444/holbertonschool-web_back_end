79e5c4f9ffc0fae4d5d4680bb9aad9cc94c18472
Brayden Vernon <mt.vernon44@gmail.com>
2023-05-08 13:55:47 +0000

    final tasks for testing

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""takes float multiplier as arg and returns a func that multiplies a float by multiplier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """type annotated func make_multiplier, takes float multiplier
+    as arg, returns a func that multiplies a float by multiplier"""
+    def mult(num: float):
+        return num * multiplier
+    return mult
---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""annotate functions params and return values with appropraite types"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ return a list of tuples that is a sequence on ints"""
+    return [(i, len(i)) for i in lst]
