ad49df67e038e1a09ad0b7524edd56bb6fd857c2
Brayden Vernon <mt.vernon44@gmail.com>
2023-05-16 16:21:18 +0000

    fighting documentation issues

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -3,8 +3,6 @@ import csv
 import math
 from typing import List, Tuple
 
-index_range = __import__('0-simple_helper_function').index_range
-
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -25,6 +23,17 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
+
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returns a tuple of size two containing
+    a start and end index corresponding to
+    the range of the indexes to return in a
+    list for those particular pagination params
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return start, end
+
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """use index_range to find correct indexes to paginate
