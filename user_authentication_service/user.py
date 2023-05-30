c203988ba0b371d196d61279bf169b549edbc5bb
Brayden Vernon <mt.vernon44@gmail.com>
2023-05-30 18:54:33 +0000

    tasks 0-2

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
-"""
-Create SQLAlchemy Model for User
-"""
+""" User module """
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
-Base = declarative_base() # Base Class for SQLAlchemy model declaration
+Base = declarative_base()  # Base class for SQLAlchemy model declaration
 
 
 class User(Base):
-    """
-    User Class Definition
-    """
-    __tablename__ = 'users' # MySQL table name
+    """ User class definition """
+    __tablename__ = 'users'  # MySQL table name
 
     id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
