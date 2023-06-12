0ffac0ac04a8ffaede4599a75471b93abff35388
Brayden Vernon <mt.vernon44@gmail.com>
2023-06-12 14:24:21 +0000

    task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -20,3 +20,20 @@ class Cache():
         self._redis.set(key, data)
         return key
 
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str,
+                                                                    bytes,
+                                                                    int,
+                                                                    float]:
+        """ Get data from redis """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ Get string data from redis """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """ Get int data from redis """
+        return self.get(key, int)
