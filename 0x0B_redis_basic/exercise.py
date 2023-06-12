408983fa756920f02bcf8cb6e4fbc83cad064c89
Brayden Vernon <mt.vernon44@gmail.com>
2023-06-12 15:39:26 +0000

    proj wrapup

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,10 +1,54 @@
 #!/usr/bin/env python3
-"""exercise.py"""
+""" Redis exercise file """
+
 
 import redis
 import uuid
 from typing import Union, Callable, Optional
 from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """ Count calls decorator """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper function """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """ Call history decorator """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper function """
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+
+        return output
+    return wrapper
+
+
+def replay(method: Callable):
+    """ Replay decorator """
+    r = redis.Redis()
+    method_name = method.__qualname__
+    count = r.get(method_name).decode("utf-8")
+    inputs = r.lrange(method_name + ":inputs", 0, -1)
+    outputs = r.lrange(method_name + ":outputs", 0, -1)
+
+    print("{} was called {} times:".format(method_name, count))
+
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(method_name, i.decode("utf-8"),
+                                     o.decode("utf-8")))
+
 
 class Cache():
     """ Cache class """
@@ -14,6 +58,8 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Store data in redis """
         key = str(uuid.uuid4())
