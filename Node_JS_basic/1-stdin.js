b88b248e32886409bc5e7b3018459030a9aaa762
Brayden Vernon <mt.vernon44@gmail.com>
2023-06-24 17:10:20 +0000

    fix to lint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,18 +2,18 @@
 // get input, and display name provided
 // when user ends program will display "This important software is now closing"
 const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
 
 readline.question("Welcome to Holberton School, what is your name?\n", function(name) {
-    console.log(`Your name is: ${name}`);
+  console.log(`Your name is: ${name}`);
 });
 
 if (!process.stdin.isTTY) {
-    process.on('exit', function(code) {
-      process.stdout.write("This important software is now closing\n");
-      process.exit(0);
-    });
+  process.on('exit', function(code) {
+    process.stdout.write("This important software is now closing\n");
+    process.exit(0);
+  });
 }
